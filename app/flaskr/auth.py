bc688cf5fd252cfc45ecdb154bf4e890a28e33c4
Nosha08 <noah.rozin@outlook.com>
2023-01-02 11:18:08 -0500

    a

--- app/flaskr/auth.py
+++ app/flaskr/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
@@ -10,6 +10,27 @@ def login():
 def logout():
     return render_template('logout.html')
 
-@auth.route('/register')
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        firstName = request.form.get('firstName')
+        lastName = request.form.get('lastName')
+        email = request.form.get('email')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 5:
+            flash('Email must be greater than 4 characters.', category='error')
+        elif len(firstName) < 1:
+            flash('Name must be greater than 0 characters.', category='error')
+        elif len(firstName) < 1:
+            flash('Name must be greater than 0 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords do not match.', category='error')
+        elif len(password1) < 8:
+            flash('Password must be at least than 8 characters.', category='error')
+        else:
+            # add user to database
+            flash('Account created!', category='success')
+
     return render_template('register.html')
