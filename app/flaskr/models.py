97eeaacca37cde433d3b9e881fb67cc5bbc5ec1d
Nosha08 <noah.rozin@outlook.com>
2023-01-02 23:32:12 -0500

    aa

--- app/flaskr/models.py
+++ app/flaskr/models.py
@@ -2,9 +2,8 @@ from flask_login import UserMixin
 from flaskr import db
 
 class User(db.Model, UserMixin):
-    def __init__(self):
-        id = db.Column(db.Integer, primary_key=True)
-        self.firstName = db.Column(db.String(150))
-        self.lastName = db.Column(db.String(150))
-        self.email = db.Column(db.String(150), unique=True)
-        self.password = db.Column(db.String(150))
+    id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String(150))
+    lastName = db.Column(db.String(150))
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
