3a2796efb8f8de16a0a270bf743a7b680da89691
Nosha08 <99495313+Nosha08@users.noreply.github.com>
2023-01-04 11:01:39 -0500

    sad

--- app/flaskr/forecast.py
+++ app/flaskr/forecast.py
@@ -2,5 +2,5 @@ import requests, json
 
 api_key = '57fc3439dc46a8578604c33691f100e3'
 
-data = requests.get('https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=44.34&lon=10.99&appid={api_key}').json()
+data = requests.get('https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=44.34&lon=10.99&appid=57fc3439dc46a8578604c33691f100e3').json()
 print(data)
