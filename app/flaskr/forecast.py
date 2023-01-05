365dfc78c4d55f050581432cd638680d7ea49c74
Nosha08 <noah.rozin@outlook.com>
2023-01-04 19:25:53 -0500

    a

--- app/flaskr/forecast.py
+++ app/flaskr/forecast.py
@@ -2,5 +2,5 @@ import requests, json
 
 api_key = '57fc3439dc46a8578604c33691f100e3'
 
-data = requests.get('https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=44.34&lon=10.99&appid=57fc3439dc46a8578604c33691f100e3').json()
+data = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat=30.34&lon=100.99&appid={api_key}').json()
 print(data)
