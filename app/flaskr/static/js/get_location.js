2386e89efdca2a96dcb79b9c9105b309fb949358
samuelkipnis <samuelk140@nycstudents.net>
2023-01-06 11:00:23 -0500

    error handling

--- app/flaskr/static/js/get_location.js
+++ app/flaskr/static/js/get_location.js
@@ -1,10 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
+function get_location() {
     location_input = document.querySelector('#location')
     console.log(location_input)
     navigator.geolocation.getCurrentPosition(pos => {
         coords = { lat: pos.coords.latitude, long: pos.coords.longitude }
         location_input.value = JSON.stringify(coords)
         console.log(location_input.value)
+}
+
+document.addEventListener('DOMContentLoaded', 
+    () => {
+        get_location()
+    }, err => {
+        alert('We need your location to display your local weather. Please accept the request.')
+        get_location()
     })
 })
 
