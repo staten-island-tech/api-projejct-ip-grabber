5e86fd00ad3bdcb39ddbde393f5bee6f900af5b2
Nosha08 <noah.rozin@outlook.com>
2023-01-08 00:59:22 -0500

    asd

--- app/flaskr/data.py
+++ app/flaskr/data.py
@@ -2,11 +2,16 @@ import requests, json
 
 api_key = '57fc3439dc46a8578604c33691f100e3'
 
-currentWeather = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat=30.34&lon=100.99&appid={api_key}').json()
+city_name = 'Anchorage'
+state_code = 'AK'
+country_code = 'US'
+locationConverter = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&limit=5&appid={api_key}').json()
+
+for x in locationConverter:
+    lat = x['lat']
+    lon = x['lon']
+
+currentWeather = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}').json()
 print(currentWeather)
 
-forecast = requests.get(f'https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid={api_key}').json()
-print(forecast)
-
-locationCovert = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={api_key}').json()
-print(locationCovert)
+forecast = requests.get(f'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={api_key}').json()
