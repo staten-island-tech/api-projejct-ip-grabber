f9bd2687b30b6314e9bea27c07b2efd779b48205
samuelkipnis <samuelk140@nycstudents.net>
2023-01-09 10:36:45 -0500

    handle user not accept location error

--- app/flaskr/static/js/get_location.js
+++ app/flaskr/static/js/get_location.js
@@ -1,18 +1,16 @@
 function get_location() {
     location_input = document.querySelector('#location')
-    console.log(location_input)
+    alert('Please allow location, it lets us show your local weather. This is required to create your account.')
     navigator.geolocation.getCurrentPosition(pos => {
         coords = { lat: pos.coords.latitude, long: pos.coords.longitude }
         location_input.value = JSON.stringify(coords)
-        console.log(location_input.value)
+    }, () => {
+        location.href = '/';
+    })
 }
 
 document.addEventListener('DOMContentLoaded', 
     () => {
         get_location()
-    }, err => {
-        alert('We need your location to display your local weather. Please accept the request.')
-        get_location()
-    })
-})
-
+    }
+)
