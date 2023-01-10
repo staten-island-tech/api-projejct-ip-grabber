2d01802923dfb96058240c426438b4738f80b425
Nosha08 <99495313+Nosha08@users.noreply.github.com>
2023-01-10 10:21:38 -0500

    sda

--- app/flaskr/views.py
+++ app/flaskr/views.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 from .models import User
+import requests
+from .data import *
 
 views = Blueprint('views', __name__)
 
@@ -9,7 +11,9 @@ views = Blueprint('views', __name__)
 def home():
     return render_template('index.html', user=current_user, firstName=User.firstName)
 
-@views.route('/forecast/<location>')
+@views.route('/forecast/<location>', methods=['GET', 'POST'])
 @login_required
 def forecast(location):
+    search = requests.form['search']
     return render_template('weather.html', location=location, user=current_user)
+
