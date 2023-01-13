6cc4df4775a3581b97ef6a646feac52194abf2b2
samuelkipnis <samuelk140@nycstudents.net>
2023-01-13 11:01:26 -0500

    forecast

--- app/flaskr/views.py
+++ app/flaskr/views.py
@@ -3,11 +3,19 @@ from flask_login import login_required, current_user
 from .models import User
 import requests
 from .data import *
-import re
+
 
 views = Blueprint('views', __name__)
 
 api_key = '57fc3439dc46a8578604c33691f100e3'
+months = {
+    '1': 'Jan', '2': 'Feb', '3': 'March', '4': 'April', '5': 'May', '6': 'June', '7': 'July', '8': 'Aug', '9': 'Sep', '10': 'Oct', '11': 'Nov', '12': 'Dec'
+}
+def convertTimeToDate(time):
+    month = months[str(int(time[6:7]))]
+    day = time[7:9]
+    date = month + day
+    return date
 
 @views.route('/')
 @login_required
@@ -19,7 +27,7 @@ def home():
 def forecast(location):
     if location == 'local':
         forecast = requests.get(f'https://api.openweathermap.org/data/2.5/forecast?lat={current_user.lat}&lon={current_user.long}&appid={api_key}').json()
-        return render_template('local_weather.html', user=current_user, forecast=forecast)
+        return render_template('local_weather.html', user=current_user, forecast=forecast, round=round, convertTimeToDate=convertTimeToDate)
     else: 
         return render_template('weather.html', location=location, user=current_user, locationConverter=locationConverter)
 # ADD GEOAPIFY API
